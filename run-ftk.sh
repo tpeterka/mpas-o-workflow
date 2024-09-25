9ca0f0c05243265e7e69e315065d97a5817ca850
Tom Peterka <tpeterka@mcs.anl.gov>
2024-09-25 08:40:33 -0500

    Added run script for ftk and ndarray yaml

--- run-ftk.sh
+++ run-ftk.sh
@@ -1,3 +1,8 @@
-ftk -f mpas-o-pt --input ./mpas.yaml --output mpas.vtp --ptgeo-seeds \
+# ftk -f mpas-o-pt --input ./mpas.yaml --output mpas.vtp --ptgeo-seeds \
+# 16,25,50,13,-15,17,1,-10,-10 --ptgeo-checkpoint-days 30 --timesteps 1 --geo \
+# --accelerator cuda --pt-delta-t  252288000
+
+henson-exec -- $HOME/climate/mpas-o-workflow/install/bin/ftk_shared.so \
+-f mpas-o-pt --input ./mpas.yaml --output mpas.vtp --ptgeo-seeds \
 16,25,50,13,-15,17,1,-10,-10 --ptgeo-checkpoint-days 30 --timesteps 1 --geo \
 --accelerator cuda --pt-delta-t  252288000
